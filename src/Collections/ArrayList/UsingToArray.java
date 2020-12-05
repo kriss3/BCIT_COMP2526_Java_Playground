3577d27642d0f1bebc0ec8a1c1e76be0a627b3ec
Kris <kris_s2@outlook.com>
2020-12-04 22:38:16 -0800

    Java, ArrayList practicing the usage of toArray executed on LinedList, strongly typed;

---
+++ src/Collections/ArrayList/UsingToArray.java
@@ -0,0 +1,28 @@
+package Collections.ArrayList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public final class UsingToArray {
+    public UsingToArray() {
+
+        String[] colors = {"black", "blue", "yellow"};
+        final LinkedList<String> links = new LinkedList<String>(Arrays.asList(colors));
+
+        links.addLast("red"); // add as last item
+        links.add("pink"); // add to the end
+        links.add(3, "green"); // add at 3rd index
+        links.addFirst("cyan"); // add as first item
+
+        colors = links.toArray(new String[links.size()]);
+
+        System.out.println("colors: ");
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+
+    public static void main(String[] args) {
+        new UsingToArray();
+    }
+}
