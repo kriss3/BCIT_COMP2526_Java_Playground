b889ba457d12bb370e02f40221873472993177ff
Kris <kris_s2@outlook.com>
2020-12-05 23:44:10 -0800

    Java, ArrayList practicing usage of Iterator object of a collection;

---
+++ src/Collections/ArrayList/IteratorExercise.java
@@ -0,0 +1,41 @@
+package Collections.ArrayList;
+
+import java.util.*;
+
+public final class IteratorExercise {
+    private static final String[] COLOURS = {"MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+    private static final String[] REMOVE_COLOURS = {"RED", "WHITE", "BLUE" };
+
+    public IteratorExercise() {
+        List<String> list = new ArrayList<>();
+        List<String> removeList = new ArrayList<>();
+        Collections.addAll(list, COLOURS);
+        Collections.addAll(removeList, REMOVE_COLOURS);
+
+        System.out.println("ArrayList: ");
+        for (String s : list) {
+            System.out.printf("%s ", s);
+        }
+
+        System.out.println("\n\nArrayList after calling removeColors: ");
+        removeColors(list, removeList);
+        for (String color : list) {
+            System.out.printf("%s ", color);
+        }
+    }
+
+    private void removeColors(
+            final Collection<String> collection1,
+            final Collection<String> collection2) {
+        Iterator<String> iterator = collection1.iterator();
+        while (iterator.hasNext()) {
+            if (collection1.contains(iterator.next())) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new IteratorExercise();
+    }
+}
