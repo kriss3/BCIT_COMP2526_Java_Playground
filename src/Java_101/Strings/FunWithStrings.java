dc2c140e8db8dd516c0742e8d05563edf34b9896
Kris <kris_s2@outlook.com>
2020-12-11 23:18:38 -0800

    Java 101: playing with Strings;

---
+++ src/Java_101/Strings/FunWithStrings.java
@@ -0,0 +1,36 @@
+package Java_101.Strings;
+
+import java.util.Scanner;
+
+public class FunWithStrings {
+
+    public static void main(String[] args) {
+
+        final Scanner titleScanner = new Scanner(System.in);
+        System.out.print("Enter the name of your favourite book: ");
+
+        final String userInput = titleScanner.nextLine();
+        // userInput = titleScanner.next();
+        System.out.println(userInput);
+
+        System.out.println("The title is " + userInput.length() + " characters long");
+        userInput.toUpperCase();
+        System.out.println(userInput);
+
+        final String upperCaseUserInput = userInput.toUpperCase();
+        System.out.println("User input in upper case: " + upperCaseUserInput);
+
+        final String trimmedUserInput = userInput.trim();
+        System.out.println("Trimmed user input: " + trimmedUserInput);
+        System.out.println("Trimmed user input length : " + trimmedUserInput.length());
+
+        String formattedOutput = userInput.trim();
+        formattedOutput = formattedOutput.toLowerCase();
+        final int length = formattedOutput.length();
+        final String firstLetter = formattedOutput.substring(0, 1).toUpperCase();
+        final String finalLetter = formattedOutput.substring(length - 1, length).toUpperCase();
+        formattedOutput = formattedOutput.substring(1, length - 1);
+        System.out.println(firstLetter + formattedOutput + finalLetter);
+
+    }
+}
